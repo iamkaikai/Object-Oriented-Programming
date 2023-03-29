af1d1b1c4a45157e8616246e35b744df44b19939
YENKAI HUANG <iamkaikai@livemail.tw>
2023-03-29 10:29:44 -0400

    initial commit

--- SA-0/src/SA0.java
+++ SA-0/src/SA0.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class SA0 {
+public class SA0 {
+    public static void  main(String[] arg){
+        for(int i=0; i<10; i++) {
+            System.out.println(i);
+        }
+    }
 }
